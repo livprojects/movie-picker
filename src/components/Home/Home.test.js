8cb6a63a3cfc5071dd03af5db724d94aeedb22df
livprojects <liv.audigane@gmail.com>
2023-01-11 10:52:43 +0100

    tests render home

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -2,7 +2,40 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Home from "./Home";
 
-it("renders welcome message", () => {
+it("renders language selector", () => {
   render(<Home />);
-  expect(screen.getByText("Dramatique")).toBeInTheDocument();
+
+  const navBarEnglish = screen.getByText("English");
+  const navBarGerman = screen.getByText("Deutsch");
+  const navBarJapanese = screen.getByText("Japonais");
+  const navBarSpanish = screen.getByText("Español");
+
+  expect(navBarEnglish).toBeInTheDocument();
+  expect(navBarGerman).toBeInTheDocument();
+  expect(navBarJapanese).toBeInTheDocument();
+  expect(navBarSpanish).toBeInTheDocument();
+});
+
+it("renders genre selector", () => {
+  render(<Home />);
+
+  const actionSelector = screen.getByText("Action");
+  const animationSelector = screen.getByText("Animation");
+  const comedySelector = screen.getByText("Comédie");
+  const crimeSelector = screen.getByText("Crime");
+
+  expect(actionSelector).toBeInTheDocument();
+  expect(animationSelector).toBeInTheDocument();
+  expect(comedySelector).toBeInTheDocument();
+  expect(crimeSelector).toBeInTheDocument();
+});
+
+it("renders movie picker navbar", () => {
+  render(<Home />);
+
+  const genreSelector = screen.getByText("Genre ?");
+  const languageSelector = screen.getByText("Langue d'origine ?");
+
+  expect(genreSelector).toBeInTheDocument();
+  expect(languageSelector).toBeInTheDocument();
 });
